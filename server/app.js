61bf759839008057f9b5b0c2402d1adf4d9e49b0
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-13 17:17:56 +0800

    add

--- server/app.js
+++ server/app.js
@@ -1,16 +1,60 @@
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "athletes",
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const dbService = require("./dbService");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// create
+app.post("/insert", (request, response) => {
+  const { name } = request.body;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.insertNewName(name);
+
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
 });
 
-connection.connect();
+// read
+app.get("/getAll", (request, response) => {
+  const db = dbService.getDbServiceInstance();
 
-connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
-  if (error) throw error;
-  console.log("The solution is: ", results[0].solution);
+  const result = db.getAllData();
+
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
 });
 
-connection.end();
+// update
+app.patch("/update", (request, response) => {
+  const { id, name } = request.body;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.updateNameById(id, name);
+
+  result
+    .then((data) => response.json({ success: data }))
+    .catch((err) => console.log(err));
+});
+
+// delete
+app.delete("/delete/:id", (request, response) => {
+  const { id } = request.params;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.deleteRowById(id);
+
+  result
+    .then((data) => response.json({ success: data }))
+    .catch((err) => console.log(err));
+});
+
+app.listen(process.env.PORT, () => console.log("app is running"));
